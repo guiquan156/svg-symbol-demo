2f3d0187706b9496615de5725e3f358921faa5a0
guiquan156 <guiquan156@163.com>
2019-05-04 20:35:58 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+    // svgRule.uses.clear();
+    svgRule
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+        .loader('svg-sprite-loader');
+  }
+};
